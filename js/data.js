54721aa825e2f45fe92f210c0c1ab2f0cb83670c
LilliamHaro <qualia950@gmail.com>
2018-01-01 16:07:51 -0500

    actualizando data para la seccion ofertas

--- js/data.js
+++ js/data.js
@@ -10,7 +10,10 @@ var data = [
     offers:
     [{image: '<img src="../assets/images/pollo-entero.jpg" alt="">',
       plate: 'pollo entero',
-      price: 's/ 48.50'}]
+      price: 's/ 48.50'},
+    {image: '<img src="../assets/images/chicha-jarra.jpg" alt="">',
+      plate: 'La jarra de chicha',
+      price: 's/ 10.90'}]
   },
   {
     name: 'Corralito',
@@ -21,10 +24,10 @@ var data = [
     map: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d975.4059211352114!2d-77.1128512411567!3d-12.069392797990762!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105cbdd223f5d73%3A0xd824ae41551d9b4c!2sCORRALITO!5e0!3m2!1ses-419!2spe!4v1514707630400',
     num: 1,
     offers:
-    [{image: '<img src="../assets/images/pollo-entero.jpg" alt="">',
+    [{image: '<img src="../assets/images/parrillada.jpg" alt="">',
       plate: 'parrilada familar',
       price: 's/ 48.50'},
-    {image: '<img src="../assets/images/pollo-entero.jpg" alt="">',
+    {image: '<img src="../assets/images/milanesa.jpeg" alt="">',
       plate: 'milanesa personal',
       price: 's/ 10.50'}]
   },
@@ -35,7 +38,14 @@ var data = [
     address: 'Cuadra 3, esquina con, Malecón Cisneros & Calle Tripoli, Miraflores 15074',
     lema: '"Más de veinte años en el mercado gastronómico peruano"',
     map: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3900.8129356134327!2d-77.0393715853076!3d-12.124948191415234!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c823d8edffdb%3A0x8225ba13c7d63a3b!2sCalle+Tripoli+%26+Malec%C3%B3n+Cisneros%2C+Miraflores+15074!5e0!3m2!1ses-419!2spe!4v1514708675802',
-    num: 2
+    num: 2,
+    offers:
+    [{image: '<img src="../assets/images/jalea.jpg" alt="">',
+      plate: 'Jalea para dos',
+      price: 's/ 28.50'},
+    {image: '<img src="../assets/images/ceviche.jpg" alt="">',
+      plate: 'Ceviche mixto para dos',
+      price: 's/ 18.50'},]
   },
   {
     name: 'Cevicheria Sara Sara',
@@ -44,7 +54,14 @@ var data = [
     address: 'Av. Los Insurgentes, La Perla 07011',
     lema: '"Especialidad el Ceviche Sara Sara"',
     map: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.6509136265195!2d-77.10625518530816!3d-12.067522591453447!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105cbe24cd88fc3%3A0x9089537c88255bdd!2sAv.+los+Insurgentes%2C+La+Perla!5e0!3m2!1ses-419!2spe!4v1514708739567',
-    num: 3
+    num: 3,
+    offers:
+    [{image: '<img src="../assets/images/ceviche.jpg" alt="">',
+      plate: 'Ceviche mixto personal',
+      price: 's/ 16.50'},
+    {image: '<img src="../assets/images/cabrilla.jpg" alt="">',
+      plate: 'Cabrilla con yucas doradas',
+      price: 's/ 28.50'}]
   },
   {
     name: 'Pepe',
@@ -53,7 +70,14 @@ var data = [
     address: 'Av.santistevan entre tungurahua y eloy alfaro',
     lema: '"Hoy es un gran día para comer ceviche"',
     map: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62433.16876081594!2d-77.12861162729263!3d-12.03849206695824!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105cef32b69fdc1%3A0xdbd8872237ace6fb!2sPepe&#39;s!5e0!3m2!1ses-419!2spe!4v1514708825117',
-    num: 4
+    num: 4,
+    offers:
+    [{image: '<img src="../assets/images/tigre.jpg" alt="">',
+      plate: 'Leche de tigre',
+      price: 's/ 9.50'},
+    {image: '<img src="../assets/images/gaseosas.jpg" alt="">',
+      plate: 'cualquier bebida personal',
+      price: 's/ 2.00'}],
   },
   {
     name: 'Sushi-Bar Tokyo',
@@ -62,7 +86,11 @@ var data = [
     address: 'Calle Berlín 645 Miraflores - Miraflores',
     lema: '"Horario de Atención: 12.30 pm. a 3.30 pm. y de 6.30 pm. a 10 pm"',
     map: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3900.8812490325467!2d-77.03848438530764!3d-12.120276791418357!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c822d89d632f%3A0xe1cf1f4ecff578bb!2sCalle+Berlin+645%2C+Miraflores+15074!5e0!3m2!1ses-419!2spe!4v1514708927481',
-    num: 5
+    num: 5,
+    offers:
+    [{image: '<img src="../assets/images/makis.jpg" alt="">',
+      plate: 'pack de 50 makis mixtos',
+      price: 's/ 30.00'}]
   },
   {
     name: 'Suchi-Bar Makoto',
@@ -71,7 +99,14 @@ var data = [
     address: 'Calle Dos de Mayo 413, Lima, Perú',
     lema: '"Prueba el delicioso paquete "SENSEI" ',
     map: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3900.913618435503!2d-77.03504378530772!3d-12.11806269141982!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c817e6dfe189%3A0x272de3cf45b053aa!2sCalle+2+de+Mayo+413%2C+Miraflores+15074!5e0!3m2!1ses-419!2spe!4v1514708976788',
-    num: 6
+    num: 6,
+    offers:
+    [{image: '<img src="../assets/images/sensei-pack.jpg" alt="">',
+      plate: 'pack Sensei de makis y bolas de arroz',
+      price: 's/ 45.00'},
+    {image: '<img src="../assets/images/gaseosas.jpg" alt="">',
+      plate: 'cualquier gaseosa personal',
+      price: 's/ 3.50'}]
   },
   {
     name: 'Rustica',
@@ -80,7 +115,15 @@ var data = [
     address: 'Av. La Marina 2631, San Miguel 15087',
     lema: '"Un deseo no cambia la vida, una decision lo cambia todo" -MDC-',
     map: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.4964809101525!2d-77.09390358530798!3d-12.078125891446481!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c97abb344483%3A0xaf99e288f12203!2sAv.+La+Marina+2631%2C+San+Miguel+15087!5e0!3m2!1ses-419!2spe!4v1514709017830',
-    num: 7
+    num: 7,
+    offers:
+    [{image: '<img src="../assets/images/seco.jpg" alt="">',
+      plate: 'Seco con flejoles personal',
+      price: 's/ 18.50'},
+    {image: '<img src="../assets/images/juanes.jpg" alt="">',
+      plate: 'Juanes',
+      price: 's/ 28.50'}]
+
   },
   {
     name: 'Chicha y Carbó',
@@ -89,6 +132,13 @@ var data = [
     address: 'Bellas Artes 200, Cercado de Lima 15021',
     lema: '"Delicioso!"',
     map: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.3748483610507!2d-77.00117398530797!3d-12.086470691440908!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c7d281893feb%3A0xbbe0526ef2018e6e!2sBellas+Artes+200%2C+Cercado+de+Lima+15021!5e0!3m2!1ses-419!2spe!4v1514709058451',
-    num: 8
+    num: 8,
+    offers:
+    [{image: '<img src="../assets/images/chicha-jarra.jpg" alt="">',
+      plate: 'La segunda jarra de chicha',
+      price: 's/ 1.00'},
+    {image: '<img src="../assets/images/chancho.jpg" alt="">',
+      plate: 'Porcion de chancho al palo crocante',
+      price: 's/ 16.50'}]
   }
 ];
